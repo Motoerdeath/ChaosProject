628134a8374ef6f0b90db994f88a230845e7a733
Frederik Scheiderbauer <36038407+Motoerdeath@users.noreply.github.com>
2025-06-20 21:55:21 +0200

    Delete build directory

--- build/CMakeFiles/ChaosCampProject.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for ChaosCampProject.
-# This may be replaced when dependencies are built.
--- build/CMakeFiles/ChaosCampProject.dir/src/crtImage.cpp.obj.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/ChaosCampProject.dir/src/crtImage.cpp.obj: \
- G:\ChaosProject\src\crtImage.cpp g:\chaosproject\headers\crtimage.hpp
