17aa090f11c6223edcbccc26655f46a1c4ccc54d
Frederik Scheiderbauer <frederik.scheiderbauer@gmx.de>
2025-06-29 22:52:46 +0200

    rework structure

--- headers/ray.hpp
+++ headers/ray.hpp
@@ -10,17 +10,19 @@ class CRTRay {
     CRTRay() : rayOrigin(0.f),rayDirection(0.f) {}
     CRTRay(CRTVector origin, CRTVector direction) :  rayOrigin(origin), rayDirection(direction) {}
     bool intersectTriangle(CRTTriangle triangle, float& t) {
-
+        CRTVector normal = triangle.calculateTriangleNormal();
         //check if Ray is not parallel and if ray hits it from the front
-        if(CRTVector::dot(rayDirection, triangle.normal) != 0) {
+        if(CRTVector::dot(rayDirection, normal) != 0) {
             //return true;
             
             //find intersection point
-            float rpLength = CRTVector::dot(triangle.v0-rayOrigin,triangle.normal);
-            float t1 = rpLength/CRTVector::dot(rayDirection,triangle.normal);
+            float rpLength = std::abs(CRTVector::dot(triangle.v0-rayOrigin,normal));
+            float t1 = rpLength/CRTVector::dot(rayDirection,normal);
+            /*
             if(t1 < 0) {
                 return false;
             }
+                */
             CRTVector intersection = rayOrigin + rayDirection*t1;
 
             CRTVector e0 = triangle.v1 - triangle.v0;
@@ -30,7 +32,7 @@ class CRTRay {
             CRTVector v1p = intersection-triangle.v1;
             CRTVector v2p = intersection-triangle.v2;
             //check if intersection point is in triangle
-            if(CRTVector::dot(triangle.normal,CRTVector::cross(e0,v0p)) >0 && CRTVector::dot(triangle.normal,CRTVector::cross(e1,v1p)) >0 && CRTVector::dot(triangle.normal,CRTVector::cross(e2,v2p)) >0 ) {
+            if(CRTVector::dot(normal,CRTVector::cross(e0,v0p)) >0 && CRTVector::dot(normal,CRTVector::cross(e1,v1p)) >0 && CRTVector::dot(normal,CRTVector::cross(e2,v2p)) >0 ) {
                 t = t1;
                 return true;
             }
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include "../src/Tasks/task1.cpp"
 #include "../src/Tasks/task2.cpp"
 #include "../src/Tasks/task3.cpp"
+#include "../src/Tasks/task4.cpp"
 
 #include "../headers/triangle.hpp"
 #include "../headers/camera.hpp"
@@ -15,7 +16,7 @@ bool RAYS = true;
 
 
 int main() {
-
+    /*
     Task1 task1;
     task1.execute();
 
@@ -25,21 +26,29 @@ int main() {
     Task3 task3;
     task3.execute();
 
-    CRTTriangle triangle(CRTVector(-2.f,-2.f,-3.f),CRTVector(1.75f,-1.75f,-3.f),CRTVector(0.f,1.75f,-3.f));
-    triangle.color = {0,255,0};
+    Task4 task4;
+    task4.execute();
+    */
+
+
+    CRTTriangle triangle1(CRTVector(-1.75f,-1.75f,-3.f),CRTVector(1.75f,-1.75f,-3.f),CRTVector(0.f,-1.75f,-3.f));
+    triangle1.color = {0,255,0};
+    std::vector<CRTTriangle> triangles;
+    triangles.push_back(triangle1);
+    
+    
     CRTTriangle triangle2(CRTVector(-2.75f,-1.75f,-2.f),CRTVector(.75f,-.75f,-4.f),CRTVector(1.5f,2.25f,-4.f));
     triangle2.color = {255,0,0};
     //CRTTriangle triangle(CRTVector(-1.75f,-1.75f,-3.f),CRTVector(1.75f,-1.75f,-3.f),CRTVector(0.f,1.75f,-3.f));
-    CRTVector cameraPosition(-2.f,-1.f,1.f);
-    std::vector<CRTTriangle> triangles;
+    
     //CRTTriangle tri3(CRTVector(-2.f,-2.f,-3.f),CRTVector(-3.f,1.f,-3.f),CRTVector(-1.f,0.f,-3.f));
     
-    triangles.push_back(triangle);
+    
     //triangles.push_back(triangle);
     //triangles.push_back(triangle2);
 
     std::vector<CRTTriangle> triangles2;
-    CRTTriangle tri1(CRTVector(-2.f,0.0f,-3.f),CRTVector(-1.f,0.f,-3.f),CRTVector(-2.f,1.f,-3.f));
+    CRTTriangle tri1(CRTVector(-1.75f,-1.75f,-3.f),CRTVector(1.75f,-1.75f,-3.f),CRTVector(0.f,-1.75f,-3.f));
     CRTTriangle tri5(CRTVector(0.f,0.0f,-3.f),CRTVector(1.f,0.f,-3.f),CRTVector(0.f,1.f,-3.f));
 
 
@@ -52,17 +61,19 @@ int main() {
     tri4.color = {255,255,255};
 
     triangles2.push_back(tri1);
-    triangles2.push_back(tri5);
+    //triangles2.push_back(tri5);
+    //triangles2.push_back(triangle1);
     //triangles2.push_back(tri2);
     //triangles2.push_back(tri3);
     //triangles2.push_back(tri4);
     std::vector<int> triangleColor = {0,255,0};
+    
     int width = 1920;
     int height = 1080;
     PPMImage image(width, height);
     //image.configureGrid(4, 4);
     //image.renderRandomizedBackground();
-    image.generateCameraRays(cameraPosition);
+    image.generateCameraRays(CRTVector(0.f));
     for(int i = 0; i < height;i++) {
         for(int j = 0; j < width;j++) {
             float lowestDistance = FLT_MAX;
@@ -78,6 +89,7 @@ int main() {
 
         }
     }
-    image.storeImageToFile("../outputs/05_Triangle2/output.ppm");
+    image.storeImageToFile("../outputs/05_Triangle/output.ppm");
+    
     return 0;
 }
